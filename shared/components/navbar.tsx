50da10c0481b06bf9a2ff60a5934c299db0f9d2b
harshgoel05 <harshgoel05@gmail.com>
2021-01-23 18:00:03 +0530

    :wrench: Update Eslint config

--- shared/components/navbar.tsx
+++ shared/components/navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { Menu } from 'react-feather';
