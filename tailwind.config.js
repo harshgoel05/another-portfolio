7f1677d4bde7e7cf2a2c4ecc0ab56e56dcda6e46
harshgoel05 <harshgoel05@gmail.com>
2021-01-23 21:25:23 +0530

    :green_heart: Fix build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,11 @@
 module.exports = {
-  purge: ['*.js'],
+  purge: [
+    './components//*.{js,ts,jsx,tsx}',
+    './components/*/*.{js,ts,jsx,tsx}',
+    './shared//*.{js,ts,jsx,tsx}',
+    './shared/components//*.{js,ts,jsx,tsx}',
+    './pages//*.{js,ts,jsx,tsx}'
+  ],
   darkMode: 'media',
   theme: {
     extend: {
