2e7665cfa8146b9d388ba93c845cc9af253f5756
harshgoel05 <harshgoel05@gmail.com>
2021-01-24 21:22:12 +0530

    💄 Add Types on Button Component

--- components/notfound/index.tsx
+++ components/notfound/index.tsx
@@ -17,14 +17,14 @@ export default function NotFoundPage(): JSX.Element {
           className="absolute right-0 top-0 w-1/2 sm:w-1/3 pointer-events-none"
         />
         <div className="fixed right-16 sm:right-32 lg:right-96 top-40 md:top-56">
-          <h3 className="text-6xl font-light text-white leading-loose pr-1">
+          <h3 className="text-7xl font-light text-white leading-loose pr-1 mb-2">
             Hey! You seem to be lost :(
           </h3>
           <h1 className="font-bold text-2xl md:text-1xl text-white leading-none">
             404. <br /> Not Found.
           </h1>
           <div className="mt-6">
-            <Button text="Back to Home" onClickHandler={() => router.push('/')} />
+            <Button type="solid" text="Back to Home" onClickHandler={() => router.push('/')} />
           </div>
         </div>
       </div>
--- shared/components/buttons.tsx
+++ shared/components/buttons.tsx
@@ -1,18 +1,23 @@
 interface Props {
   text: string;
+  type: 'solid' | 'outlined';
   onClickHandler: () => void;
 }
-export default function Button({ text, onClickHandler }: Props): JSX.Element {
+export default function Button({ text, type, onClickHandler }: Props): JSX.Element {
   return (
     <>
       <button
         type="button"
         onClick={onClickHandler}
-        className="bg-pink text-blue font-semibold
+        className={`font-semibold
                     p-6 py-4 w-64  text-7xl h-25 transition-all
                     rounded overflow-hidden border-2 border-transparent
-                    hover:outline-none hover:bg-transparent hover:text-pink hover:border-pink
-                    focus:outline-none focus:bg-transparent focus:text-pink focus:border-pink">
+                    ${
+                      type === 'solid'
+                        ? 'bg-pink text-blue  hover:outline-none hover:bg-transparent hover:text-pink hover:border-pink focus:outline-none focus:bg-transparent focus:text-pink focus:border-pink'
+                        : 'text-pink bg-transparent border-pink hover:outline-none hover:bg-pink hover:text-blue hover:border-pink focus:outline-none focus:bg-pink focus:text-blue focus:border-pink'
+                    }
+                   `}>
         {text}
       </button>
     </>
