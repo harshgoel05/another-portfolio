8178ca380a4f2f3907dae6ea7f39fbb4b86b2645
harshgoel05 <harshgoel05@gmail.com>
2021-01-25 19:24:47 +0530

    🔀 Fix Merge Issues

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,6 +1,8 @@
 import Landing from './landing';
 import Skills from './skills';
 
+// ml-4 sm:mx-12 md:mx-16
+
 const HomePage = (): JSX.Element => {
   return (
     <>
--- components/home/landing.tsx
+++ components/home/landing.tsx
@@ -13,7 +13,7 @@ export default function Landing(): JSX.Element {
         alt="Triangle 3d Vector"
         className="block md:hidden absolute right-1/3 top-16 w-1/4 md:w-16 animate-spin pointer-events-none"
       />
-      <div className="grid grid-cols-12 gap-4 h-84.5vh">
+      <div className="grid grid-cols-12 gap-4 h-84.5vh ml-4 sm:mx-12 md:mx-16">
         <div className="col-span-12 md:col-span-8 lg:col-span-6 flex flex-col justify-center items-center">
           {/* Hero Header */}
           <div className="items-center w-3/4 -mt-10 relative">
