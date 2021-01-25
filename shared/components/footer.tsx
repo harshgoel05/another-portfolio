962710fefd17ed4f05f1f4b2cebc4f6333daaebd
harshgoel05 <harshgoel05@gmail.com>
2021-01-25 22:03:13 +0530

    ✨ Add Skills Section in HomePage

--- shared/components/footer.tsx
+++ shared/components/footer.tsx
@@ -1,16 +1,16 @@
 export default function Footer(): JSX.Element {
   return (
     <>
-      <div className="w-full mx-auto text-white h-14 bg-blue  flex justify-center items-center">
-        <hr className="w-96 border-violet" />
-        <p className="mx-6">
+      <div className="w-full mx-auto py-4 text-white h-auto bg-blue  flex justify-center items-center">
+        <hr className="w-1/5 sm:w-1/4 lg:w-96 border-violet" />
+        <p className="mx-3 md:mx-6 text-7xl font-light">
           Made
           <span role="img" aria-label="emoji" className="mx-3 animate-pulse">
             ❤️
           </span>
           with by Harsh Goel
         </p>
-        <hr className="w-96 border-violet" />
+        <hr className="w-1/5 sm:w-1/4 lg:w-96 border-violet" />
       </div>
     </>
   );
