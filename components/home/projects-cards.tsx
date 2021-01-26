b37ab86c5f1413c296865a3dfbccaad6ee39db97
harshgoel05 <harshgoel05@gmail.com>
2021-01-26 17:18:42 +0530

    💬 Add Projects List

--- components/home/projects-cards.tsx
+++ components/home/projects-cards.tsx
@@ -6,21 +6,22 @@ export default function ProjectsCards(): JSX.Element {
     <>
       <div className="relative h-auto sm:h-auto md:h-auto ml-4 sm:mx-12 md:mx-16">
         <div className="sm:flex sm:flex-wrap justify-center items-center ">
-          {Projects.map((project: Project) => (
-            <div
-              key={project.alt}
-              className="flex flex-shrink-0 flex-grow-0 justify-center items-center sm:w-2/5 md:w-1/2 lg:w-2/5 transition-all duration-300 transform hover:opacity-80 hover:scale-110">
-              <a href={project.url} className="flex flex-1 justify-center items-center">
-                <img
-                  src={project.img}
-                  alt={project.alt}
-                  className="rounded-2xl p-6 py-3 md:p-10 lg:p-14"
-                />
-              </a>
-            </div>
-          ))}
+          {Projects.map(
+            (project: Project) =>
+              project.featured && (
+                <div
+                  key={project.name}
+                  className="flex flex-shrink-0 m-16 sm:m-10 sm:w-1/3 my-3 md:m-10 lg:mx-14 lg:my-6 
+              rounded-2xl flex-grow-0 justify-center items-center 
+              transition-all duration-300 transform hover:opacity-80 hover:scale-110 hover:shadow-violet-5xl">
+                  <a href={project?.url} className="flex flex-1 justify-center items-center">
+                    <img src={project.img} alt={project.name} className="rounded-2xl" />
+                  </a>
+                </div>
+              )
+          )}
         </div>
-        <div className="flex justify-center">
+        <div className="flex justify-center mt-4">
           <Button type="solid" text="Show me more! 🔥" />
         </div>
       </div>
