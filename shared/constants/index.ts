d8f2f8c9481b337f4a362e3c809b24e494df184f
harshgoel05 <harshgoel05@gmail.com>
2021-01-27 19:48:47 +0530

    💄 Add Work Details

--- shared/constants/index.ts
+++ shared/constants/index.ts
@@ -1,8 +1,9 @@
 import SocialMedia from './socialmedia';
 import NavLinks from './navlinks';
 import { Project, Projects } from './projects';
+import { Companies, Company } from './companies';
 
-export { SocialMedia, NavLinks, Projects };
-export type { Project };
+export { SocialMedia, NavLinks, Projects, Companies };
+export type { Project, Company };
 
 export const RESUME_URL = 'bit.ly/harshgoelresume';
