e38264085c5aa346344a29c45354e639fe550e4d
harshgoel05 <harshgoel05@gmail.com>
2021-01-31 12:38:35 +0530

    ✨ Add APIs to fetch content

--- pages/api/health.ts
+++ pages/api/health.ts
@@ -2,8 +2,12 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const date: Date = new Date();
 export default (req: NextApiRequest, res: NextApiResponse): void => {
-  const uptime = +new Date() - +date;
-
-  res.statusCode = 200;
-  res.json({ name: 'Harsh Goel', uptime, startDate: date, currentDate: new Date() });
+  try {
+    const uptime = +new Date() - +date;
+    res.statusCode = 200;
+    res.json({ name: 'Harsh Goel', uptime, startDate: date, currentDate: new Date() });
+  } catch (err) {
+    res.statusCode = 500;
+    res.json({ success: false, error: err });
+  }
 };
---
+++ pages/api/projects.ts
@@ -0,0 +1,16 @@
+import { MongoClient } from 'mongodb';
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getDbClient } from '../../api/utils/db';
+
+let dbClient: MongoClient;
+export default async (req: NextApiRequest, res: NextApiResponse): Promise<void> => {
+  try {
+    dbClient = await getDbClient();
+    const data = await dbClient.db().collection('projects').find({}).project({ _id: 0 }).toArray();
+    res.statusCode = 200;
+    res.json({ data });
+  } catch (err) {
+    res.statusCode = 500;
+    res.json({ success: false, error: err });
+  }
+};
