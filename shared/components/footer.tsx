7476b9b1b1c35b588c5faa1a5e6a73a8aeb33e41
harshgoel05 <harshgoel05@gmail.com>
2021-02-01 10:12:09 +0530

    💬 Update Footer

--- shared/components/footer.tsx
+++ shared/components/footer.tsx
@@ -3,18 +3,18 @@ import { PersonalDetailsContext } from '@utils/contexts';
 import { PersonalDetails } from '@utils/types';
 
 export default function Footer(): JSX.Element {
-  const personalDetails: PersonalDetails = useContext(PersonalDetailsContext);
+  const { name }: PersonalDetails = useContext(PersonalDetailsContext);
 
   return (
     <>
       <div className="w-full mx-auto py-4 text-white h-auto bg-blue flex justify-center items-center">
         <hr className="w-1/5 sm:w-1/4 lg:w-96 border-violet" />
         <p className="mx-3 md:mx-6 text-sm sm:text-7xl font-light">
-          Made
+          Made with
           <span role="img" aria-label="emoji" className="mx-2 animate-pulse">
             ❤️
           </span>
-          with by <span className="text-pink">{personalDetails.name}</span>
+          by <span className="text-pink">{name}</span>
         </p>
         <hr className="w-1/5 sm:w-1/4 lg:w-96 border-violet" />
       </div>
