5538a8e4019784206a828c3961491e111a61681e
harshgoel05 <harshgoel05@gmail.com>
2021-06-25 11:22:08 +0530

    ✨ Add hotjar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -128,6 +128,19 @@ function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
               gtag('config', 'G-2595CLJE11');`
           }}
         />
+        <script>
+          dangerouslySetInnerHTML=
+          {{
+            __html: `(function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2472275,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`
+          }}
+        </script>
       </Head>
       {loading ? (
         <Loader />
