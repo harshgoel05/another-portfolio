9ecf52f3e0bf1be1127d4d8b36b8b5f48c1809dd
Operel <oriperelman@gmail.com>
2021-10-16 00:32:32 +0300

    :necktie: Save user preferences to local storage. Issue #79 Refactor dismiss button to left-bottom of the alert.

--- shared/components/cookie-alert.tsx
+++ shared/components/cookie-alert.tsx
@@ -1,16 +1,40 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 
+interface CookiePreferences {
+  acceptedCookies: boolean;
+  askAgainDate: number | null;
+}
+
 export default function CookieAlert(): JSX.Element {
-  const [isOpen, setIsOpen] = useState<boolean>(true);
-  const handleAcceptCookies = (): void => {
-    setIsOpen(false);
+  const [showAlert, setShowAlert] = useState<boolean>(false);
+
+  const handleCookiesAlert = (accepted: boolean): void => {
+    setShowAlert(false);
+    const cookiePreferencesObj: CookiePreferences = {
+      acceptedCookies: accepted,
+      askAgainDate: accepted
+        ? null // user accepted cookies, no need to ask again
+        : new Date().getTime() + 1000 * 60 * 60 * 24 * 5 // current time + 5 days
+    };
+    localStorage.setItem('cookieObject', JSON.stringify(cookiePreferencesObj));
   };
-  const handleCloseAlert = (): void => {
-    setIsOpen(false);
-  };
+
+  useEffect(() => {
+    const cookieObjJson = localStorage.getItem('cookieObject');
+    if (cookieObjJson) {
+      const { acceptedCookies, askAgainDate } = JSON.parse(cookieObjJson);
+      if (!acceptedCookies && new Date().getTime() > askAgainDate) {
+        setShowAlert(true);
+      }
+    } else {
+      setShowAlert(true);
+    }
+  }, []);
+
   return (
     <motion.div
+      key="cookieAlertComponent"
       className={`fixed
                   bottom-0 md:bottom-5
                   right-0 md:right-5
@@ -22,42 +46,42 @@ export default function CookieAlert(): JSX.Element {
                 `}
       initial={{ y: '150%' }}
       animate={{
-        y: isOpen ? 0 : '150%',
-        transitionEnd: { display: isOpen ? 'block' : 'none' }
+        y: showAlert ? 0 : '150%',
+        transitionEnd: { display: showAlert ? 'block' : 'none' }
       }}
-      transition={{ delay: 1, duration: 0.5 }}>
-      <button
-        type="button"
-        className={`absolute top-2 right-2
-                    border border-blue rounded
-                    px-1 py-0
-                    hover:bg-blue hover:text-pink
-                    focus:outline-none
-                  `}
-        onClick={handleCloseAlert}>
-        X
-      </button>
+      transition={{ delay: 0.8, duration: 0.5 }}>
       <h5 className="text-lg py-2">&#x1F36A; Do you like cookies?</h5>
       <p className="py-2">We use cookies to ensure you get the best experience on our website.</p>
-      <div className="flex justify-end mt-2.5">
-        <a
-          className="px-4 py-3 hover:underline"
-          href="https://www.cookiesandyou.com/"
-          target="_blank"
-          rel="noreferrer noopener">
-          Learn More
-        </a>
+      <div className="flex justify-between mt-2.5">
         <button
-          className={`px-6 py-2
-                      bg-blue text-pink
-                      border-2 border-blue rounded
-                      hover:bg-transparent hover:text-blue
+          type="button"
+          className={`px-1 py-0
+                      hover:underline
                       focus:outline-none
                     `}
-          type="button"
-          onClick={handleAcceptCookies}>
-          Accept
+          onClick={() => handleCookiesAlert(false)}>
+          Dismiss
         </button>
+        <div>
+          <a
+            className="px-4 py-3 hover:underline"
+            href="https://www.cookiesandyou.com/"
+            target="_blank"
+            rel="noreferrer noopener">
+            Learn More
+          </a>
+          <button
+            className={`px-6 py-2
+                        bg-blue text-pink
+                        border-2 border-blue rounded
+                        hover:bg-transparent hover:text-blue
+                        focus:outline-none
+                      `}
+            type="button"
+            onClick={() => handleCookiesAlert(true)}>
+            Accept
+          </button>
+        </div>
       </div>
     </motion.div>
   );
