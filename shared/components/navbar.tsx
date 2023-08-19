08cfe0c6ec88a66caf41fb91e0944143f0651981
Harsh Goel <harshgoel05@gmail.com>
2023-08-19 13:05:24 +0530

    chore: upgrade next to 13

--- shared/components/navbar.tsx
+++ shared/components/navbar.tsx
@@ -15,25 +15,23 @@ const NavItem = ({ href, title }: Props): JSX.Element => {
   const router = useRouter();
   return (
     <Link href={href}>
-      <a>
-        <li
-          className={`
+      <li
+        className={`
           px-5 py-2 pt-1 md:pt-4 md:px-4 md:text-center
           ${router.asPath === href ? 'border-b-2 md:border-none' : ''} 
           `}>
-          <p className="transition duration-500 ease-in-out transform md:hover:-translate-y-2 text-white font-bold md:hover:text-violet">
-            {title}
-          </p>
-          <img
-            src="/images/vectors/nav_active.svg"
-            alt="nav-active"
-            className={`
+        <p className="transition duration-500 ease-in-out transform md:hover:-translate-y-2 text-white font-bold md:hover:text-violet">
+          {title}
+        </p>
+        <img
+          src="/images/vectors/nav_active.svg"
+          alt="nav-active"
+          className={`
             hidden md:block md:opacity-0
             ${router.asPath === href && 'md:opacity-100 md:w-100'}
             `}
-          />
-        </li>
-      </a>
+        />
+      </li>
     </Link>
   );
 };
